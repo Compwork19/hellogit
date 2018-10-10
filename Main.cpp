4274aa70004a970e1e5ffee3649488d74e5be350
Compwork19 <Compwork19@noreply.github.com>
2018-10-10 16:33:30 -0400

    Added file

--- Main.cpp
+++ Main.cpp
@@ -1,16 +1,28 @@
 #include<iostream>
 #include<vector>
 #include "Data.h"
+
+void bubbleSort(auto& D)
+{
+	for(int i=0;i<D.size();i++)
+		for(int j=0;j<D.Size()-1;i++)
+		if(D[j]>D[j+1])
+			swap(D[j]>D[j+1]);
+}
+
 int main()
 {
 
 vector<int>v1;
+vector<int>v;
 vector<int>v2={5,8,23};
 vi.push_back(12);
 v2.push_back(17);
 v2.pop_back
+
 cout<<"v2 has "<<v2.size()<<" elements";
 
+bubbleSort(v);
 for(auto x:v2)
 cout<<x<<endl;
 	
